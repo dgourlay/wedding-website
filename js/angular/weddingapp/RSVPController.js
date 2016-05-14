21982a3cc73b7eeb44ae5e06432a783df6995bb8
Derek Gourlay <dgourlay@amazon.com>
2016-05-14 15:19:26 -0700

    Remove excess logic around radio buttons.

--- js/angular/weddingapp/RSVPController.js
+++ js/angular/weddingapp/RSVPController.js
@@ -1,22 +1,6 @@
 angular.module('WeddingApp', [])
   .controller('RSVPController', function ($scope) {
 
-    $scope.guestAttending = false;
-    $scope.groupAccommodation = true;
-
-    $scope.changeGuestAttending = function (value) {
-      if (value === 'true') {
-        $scope.guestAttending = true;
-      } else {
-        $scope.guestAttending = false;
-      }
-    }
-
-    $scope.changeGroupAccommodation = function (value) {
-      if (value === 'true') {
-        $scope.groupAccommodation = true;
-      } else {
-        $scope.groupAccommodation = false;
-      }
-    }
+    $scope.guestAttending = 'false';
+    $scope.groupAccommodation = 'true';
   });
